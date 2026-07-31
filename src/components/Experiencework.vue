c6ed5195bd7cd37aba914996b305bb9d42e977b6
Rosida Dewi Utami <119984631+rosidadewi@users.noreply.github.com>
2026-07-31 13:06:54 +0700

    Update Experiencework.vue

--- src/components/Experiencework.vue
+++ src/components/Experiencework.vue
@@ -12,7 +12,7 @@ const experiences = [
     company: 'Dinas Komunikasi dan Informatika Kota Madiun',
     period: 'Maret 2025 — April 2025',
     desc: 'Mengembangkan antarmuka sistem informasi kepegawaian untuk instansi Diskominfo Kota Madiun selama masa Praktek Kerja Nyata. Menyusun tampilan halaman menggunakan template Blade (Laravel) yang responsif dan konsisten dengan kebutuhan pengguna instansi.',
-    tags: ['Vue.js', 'Tailwind', 'Laravel Blade'],
+    tags: ['Vue.js', 'Tailwind', 'REST API','Laravel Blade'],
   },
   {
     type: 'organisasi',
